5fef54967658772f5ff5c2af6e713768dbd59424
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-02-07 19:04:28 -0200

    small refactor: removed everything from __init__.py. demo.py more friendly too

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import sys
 from lv2 import Bundle
 from pprint import pprint as pp
 
-invada = Bundle('/usr/lib/lv2/invada.lv2')
-pp(invada.data['plugins'])
+if len(sys.argv) < 2:
+    print "Usage: demo.py bundle_path"
+    print "   ex: demo.py /usr/lib/lv2/invada.lv2"
+    sys.exit(1)
+
+bundle = Bundle(sys.argv[1])
+pp(bundle.data['plugins'])
